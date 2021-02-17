0bb8cac07fb6a3cff786669d4d039a6c6850529c
gmcvitti <gmcvitti@mathworks.com>
2021-02-17 11:10:49 -0500

    Updatd test for EXec.

--- +test/+docker/Exec.m
+++ +test/+docker/Exec.m
@@ -30,6 +30,24 @@ classdef Exec < matlab.unittest.TestCase
             docker.rm("MyArchContainer","force",true);     
         end 
         
+        function exec_in_container_w_env_var(test)
+            
+            [filepath,~,~] = fileparts(mfilename('fullpath'));
+            docker.build(fullfile(filepath,"dockerfiles","envvar"),"tag","myimage:latest");
+            
+            docker.run("myimage:latest","tail","-f /dev/null",...                
+                "detach",true,...
+                "name","MyArchContainer");
+            
+            test.verifyEqual(docker.exec("MyArchContainer",...
+                "sh","-c ""echo ${MYENVVAR}""",...
+                "env","MYENVVAR=""This is my env variable""",...
+                "workdir","/home"),"This is my env variable"+newline());
+                        
+            docker.rm("MyArchContainer","force",true);
+            docker.rmi("myimage:latest");    
+        end
+        
         function throw_error_in_exec(test)
             docker.run("archlinux:latest","tail","-f /dev/null","detach",true,"name","MyArchContainer");
             
